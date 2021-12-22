8793aeaf9be46e380abb7de39a736b8823613b89
yaswanth-pula-skyflow <yaswanthsai.pula@skyflow.com>
2021-12-22 11:17:58 +0530

    update samples

--- samples/src/main/java/com/example/TokenGenerationExample.java
+++ samples/src/main/java/com/example/TokenGenerationExample.java
@@ -7,12 +7,23 @@ import com.skyflow.serviceaccount.util.Token;
 public class TokenGenerationExample {
     public static void main(String args[]) {
 
+        // Generate BearerToken by specifying credentials.json file path
         try {
             String filePath = "<your_credentials.json_file_path>";
-            ResponseToken res = Token.GenerateToken(filePath);
+            ResponseToken res = Token.GenerateBearerToken(filePath);
             System.out.println(res.getTokenType() + ":" + res.getAccessToken());
         } catch (SkyflowException e) {
             e.printStackTrace();
         }
+
+        // Generate BearerToken by specifying credentials.json as string
+        try {
+            String filePath = "<your_credentials.json_file_as string>";
+            ResponseToken res = Token.GenerateBearerTokenFromCreds(filePath);
+            System.out.println(res.getTokenType() + ":" + res.getAccessToken());
+        } catch (SkyflowException e) {
+            e.printStackTrace();
+        }
+
     }
 }
