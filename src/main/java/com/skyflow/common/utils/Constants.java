378ce413622d868a3e0c8507878fbda82acbbaed
yaswanth-pula-skyflow <yaswanthsai.pula@skyflow.com>
2023-08-09 17:04:28 +0530

    SK-897 update pom.xml for the latest version.

--- src/main/java/com/skyflow/common/utils/Constants.java
+++ src/main/java/com/skyflow/common/utils/Constants.java
@@ -3,6 +3,6 @@ package com.skyflow.common.utils;
 public class Constants {
 
     public static final String SDK_METRICS_HEADER_KEY = "sky-metadata";
-    public static final String SDK_VERSION = "1.9.0";
+    public static final String SDK_VERSION = "1.10.0";
 
 }
