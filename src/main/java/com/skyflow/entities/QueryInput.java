3f5688932533dc8621ed6cad4c3358c68771a6fc
skyflow-lipsa <lipsa.pradhan@zemosolabs.com>
2023-08-30 17:03:09 +0530

    SK-990 tdd query support in java sdk

--- src/main/java/com/skyflow/entities/QueryInput.java
+++ src/main/java/com/skyflow/entities/QueryInput.java
@@ -1,13 +1,13 @@
 package com.skyflow.entities;
 
 public class QueryInput {
-    private QueryRecordInput records;
+    private QueryRecordInput query;
 
     public QueryRecordInput getRecords() {
-        return records;
+        return query;
     }
 
-    public void setRecords(QueryRecordInput records) {
-        this.records = records;
+    public void setRecords(QueryRecordInput query) {
+        this.query = query;
     }
 }
--- src/main/java/com/skyflow/entities/QueryOptions.java
+++ src/main/java/com/skyflow/entities/QueryOptions.java
@@ -5,17 +5,4 @@ package com.skyflow.entities;
 
 public class QueryOptions {
 
-    private boolean tokens;
-
-    public QueryOptions() {
-        this.tokens = true;
-    }
-
-    public QueryOptions(boolean tokens) {
-        this.tokens = tokens;
-    }
-
-    public boolean isTokens() {
-        return tokens;
-    }
 }
