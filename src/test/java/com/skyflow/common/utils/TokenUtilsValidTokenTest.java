a27a45ed782d5c72c0dc909ca170ffe03714dda7
yaswanth-pula-skyflow <yaswanthsai.pula@skyflow.com>
2024-01-09 12:03:08 +0530

    SK-1212 update tokenqutils expired bearer token error case.

--- src/test/java/com/skyflow/common/utils/TokenUtilsValidTokenTest.java
+++ src/test/java/com/skyflow/common/utils/TokenUtilsValidTokenTest.java
@@ -5,18 +5,13 @@ import com.skyflow.errors.ErrorCode;
 import com.skyflow.errors.SkyflowException;
 import org.apache.commons.codec.binary.Base64;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.ParseException;
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-
 import static org.junit.Assert.assertEquals;
 
 class ValidTokenProvider implements TokenProvider {
@@ -38,14 +33,14 @@ class MockDataProvider  {
 
     public byte[] returnValidDecodeBytes(){
         JSONObject validJson = new JSONObject();
-        validJson.put("exp", System.currentTimeMillis()+3000 / 1000L);
+        validJson.put("exp", (System.currentTimeMillis()+3000) / 1000);
         byte[] encodedBytes = Base64.encodeBase64(validJson.toJSONString().getBytes());
         return Base64.decodeBase64(encodedBytes);
 
     }
     public byte[] returnInValidDecodeBytes(){
         JSONObject expiredJson = new JSONObject();
-        expiredJson.put("exp", System.currentTimeMillis()-3000 / 1000L);
+        expiredJson.put("exp", (System.currentTimeMillis()-3000) / 1000);
         byte[] encodedByte = Base64.encodeBase64(expiredJson.toJSONString().getBytes());
         return Base64.decodeBase64(encodedByte);
 
@@ -56,37 +51,37 @@ class MockDataProvider  {
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(fullyQualifiedNames = "org.apache.commons.codec.binary.Base64")
 public class TokenUtilsValidTokenTest {
-    private static TokenUtils tokenUtils;
-    @BeforeClass
-    public static void setup() throws SkyflowException, ParseException {
+
+    @Before
+    public void init(){
         MockDataProvider dd = new MockDataProvider();
         byte[] validDecodeBytes = dd.returnValidDecodeBytes();
         byte[] inValidDecodeBytes = dd.returnInValidDecodeBytes();
         PowerMockito.mockStatic(Base64.class);
         PowerMockito.when(Base64.decodeBase64("valid_token")).thenReturn(validDecodeBytes);
         PowerMockito.when(Base64.decodeBase64("invalid_token")).thenReturn(inValidDecodeBytes);
-        tokenUtils = new TokenUtils();
     }
-
     @Test
     public void testValidToken() {
         try {
+            TokenUtils tokenUtils = new TokenUtils();
             String token = tokenUtils.getBearerToken(new ValidTokenProvider());
             assertEquals(token,"aa.valid_token.dd");
             String secondToken = tokenUtils.getBearerToken(new ValidTokenProvider());
             assertEquals(secondToken,"aa.valid_token.dd");
         } catch (SkyflowException e) {
-            Assert.fail("EXCEPTION THROWN!!");
+            Assert.fail("Should not throw EXCEPTION !!");
         }
     }
     
     @Test
     public void testInValidToken() {
         try {
+            TokenUtils tokenUtils = new TokenUtils();
             String token = tokenUtils.getBearerToken(new InvalidBearerTokenProvider());
             Assert.fail("should throw execution");
         } catch (SkyflowException e) {
-            assertEquals(e.getMessage(), ErrorCode.InvalidBearerToken.getDescription());
+            assertEquals(e.getMessage(), ErrorCode.BearerTokenExpired.getDescription());
 
         }
     }
