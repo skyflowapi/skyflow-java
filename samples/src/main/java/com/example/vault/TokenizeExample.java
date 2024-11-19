b2b06c1936b78acffc0e5607a3297948eb7a0acb
skyflow-vivek <vivek.jain@skyflow.com>
2024-11-19 19:15:29 +0530

    SK-1685 Add samples for Java SDK v2 - Added samples for vault API interfaces - Added sample for invoke connection interface

---
+++ samples/src/main/java/com/example/vault/TokenizeExample.java
@@ -0,0 +1,60 @@
+package com.example.vault;
+
+import com.skyflow.Skyflow;
+import com.skyflow.config.Credentials;
+import com.skyflow.config.VaultConfig;
+import com.skyflow.enums.Env;
+import com.skyflow.enums.LogLevel;
+import com.skyflow.errors.SkyflowException;
+import com.skyflow.vault.tokens.ColumnValue;
+import com.skyflow.vault.tokens.TokenizeRequest;
+import com.skyflow.vault.tokens.TokenizeResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TokenizeExample {
+    public static void main(String[] args) throws SkyflowException {
+        Credentials credentials = new Credentials();
+        credentials.setPath("<path_to_your_credentials_file_1>");
+
+        VaultConfig blitzConfig = new VaultConfig();
+        blitzConfig.setVaultId("<your_vault_id_1>");
+        blitzConfig.setClusterId("<your_cluster_id_1>");
+        blitzConfig.setEnv(Env.DEV);
+        blitzConfig.setCredentials(credentials);
+
+        VaultConfig stageConfig = new VaultConfig();
+        stageConfig.setVaultId("<your_vault_id_2>");
+        stageConfig.setClusterId("<your_cluster_id_2>");
+        stageConfig.setEnv(Env.STAGE);
+
+        Credentials skyflowCredentials = new Credentials();
+        credentials.setPath("<path_to_your_credentials_file_2>");
+
+        Skyflow skyflowClient = Skyflow.builder()
+                .setLogLevel(LogLevel.DEBUG)
+                .addVaultConfig(blitzConfig)
+                .addVaultConfig(stageConfig)
+                .addSkyflowCredentials(skyflowCredentials)
+                .build();
+
+        List<ColumnValue> columnValues1 = new ArrayList<>();
+        ColumnValue value1 = ColumnValue.builder().value("<your_value>").columnGroup("<your_column_group>").build();
+        ColumnValue value2 = ColumnValue.builder().value("<your_value>").columnGroup("<your_column_group>").build();
+        columnValues1.add(value1);
+        columnValues1.add(value2);
+        TokenizeRequest tokenizeRequest1 = TokenizeRequest.builder().values(columnValues1).build();
+        TokenizeResponse tokenizeResponse1 = skyflowClient.vault().tokenize(tokenizeRequest1);
+        System.out.println(tokenizeResponse1);
+
+        List<ColumnValue> columnValues2 = new ArrayList<>();
+        ColumnValue value3 = ColumnValue.builder().value("<your_value>").columnGroup("<your_column_group>").build();
+        ColumnValue value4 = ColumnValue.builder().value("<your_value>").columnGroup("<your_column_group>").build();
+        columnValues2.add(value3);
+        columnValues2.add(value4);
+        TokenizeRequest tokenizeRequest2 = TokenizeRequest.builder().values(columnValues2).build();
+        TokenizeResponse tokenizeResponse2 = skyflowClient.vault("<your_vault_id_2>").tokenize(tokenizeRequest2);
+        System.out.println(tokenizeResponse2);
+    }
+}
