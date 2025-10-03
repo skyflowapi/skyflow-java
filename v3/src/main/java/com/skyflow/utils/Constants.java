df42a766e507d8f4344bd92a468d830d90afa2e1
skyflow-bharti <118584001+skyflow-bharti@users.noreply.github.com>
2025-10-03 13:24:51 +0530

    SK-2305 Release/25.9.4 update code in v3 (#250)
    * SK-2305 Fix log prefix
    
    * [AUTOMATED] Public Release - 3.0.0-beta.6
    
    ---------
    
    Co-authored-by: skyflow-vivek <vivek.jain@skyflow.com>
    Co-authored-by: skyflow-vivek <skyflow-vivek@users.noreply.github.com>

--- v3/src/main/java/com/skyflow/utils/Constants.java
+++ v3/src/main/java/com/skyflow/utils/Constants.java
@@ -2,7 +2,7 @@ package com.skyflow.utils;
 
 public final class Constants extends BaseConstants {
     public static final String SDK_NAME = "Skyflow Java SDK ";
-    public static final String SDK_VERSION = "3.0.0-beta.4";
+    public static final String SDK_VERSION = "3.0.0-beta.6";
     public static final String VAULT_DOMAIN = ".skyvault.";
     public static final String SDK_PREFIX = SDK_NAME + SDK_VERSION;
     public static final Integer INSERT_BATCH_SIZE = 50;
