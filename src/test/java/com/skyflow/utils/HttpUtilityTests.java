b2a720b3eed66797bae34f9c4beae80fa556acf7
skyflow-himanshu <himanshu.rahul@skyflow.com>
2026-02-11 11:49:28 +0530

    SK-2527: addressed copilot suggestions

--- src/test/java/com/skyflow/utils/HttpUtilityTests.java
+++ src/test/java/com/skyflow/utils/HttpUtilityTests.java
@@ -6,7 +6,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
