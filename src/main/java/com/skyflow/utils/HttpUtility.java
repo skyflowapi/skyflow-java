a0c1b8752ec676e39240be09130e3066f7af8da3
Devesh-Skyflow <devesh.bhardwaj@skyflow.com>
2026-05-25 14:28:25 +0530

    Revert "fix: do not generate SDK-side requestId when server omits x-request-id header"
    This reverts commit 2740de4c22592d208b253f2763136d26b070e4ce.

--- src/main/java/com/skyflow/utils/HttpUtility.java
+++ src/main/java/com/skyflow/utils/HttpUtility.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
 public final class HttpUtility {
 
@@ -73,7 +74,11 @@ public final class HttpUtility {
 
             int httpCode = connection.getResponseCode();
             String requestID = connection.getHeaderField("x-request-id");
-            HttpUtility.requestID = requestID != null ? requestID.split(",")[0] : null;
+            if (requestID != null) {
+                HttpUtility.requestID = requestID.split(",")[0];
+            } else {
+                HttpUtility.requestID = Constants.HttpUtilityExtra.SDK_GENERATED_PREFIX + UUID.randomUUID();
+            }
             Map<String, List<String>> responseHeaders = connection.getHeaderFields();
             Reader streamReader;
             if (httpCode > 299) {
