06b45743a0d5a2718836786af1abe37d4a91edae
Devesh-Skyflow <devesh.bhardwaj@skyflow.com>
2026-08-18 19:12:00 +0530

    Revert "SK-3061: fix bulkInsert/bulkInsertAsync's inconsistent exception hand…"
    This reverts commit 291cdd3cf23b489a15322d000fdd4556fbaba0e8.

--- flowvault/src/main/java/com/skyflow/vault/controller/VaultController.java
+++ flowvault/src/main/java/com/skyflow/vault/controller/VaultController.java
@@ -157,12 +157,6 @@ public final class VaultController extends VaultClient {
             String bodyString = gson.toJson(e.body());
             LogUtil.printErrorLog(ErrorLogs.INSERT_RECORDS_REJECTED.getLog());
             throw new SkyflowException(e.statusCode(), e, e.headers(), bodyString);
-        } catch (SkyflowException e) {
-            LogUtil.printErrorLog(ErrorLogs.INSERT_RECORDS_REJECTED.getLog());
-            throw e;
-        } catch (Exception e) {
-            LogUtil.printErrorLog(ErrorLogs.INSERT_RECORDS_REJECTED.getLog());
-            throw new SkyflowException(e.getMessage());
         }
     }
 
@@ -769,9 +763,9 @@ public final class VaultController extends VaultClient {
     ) throws ExecutionException, InterruptedException, SkyflowException {
         LogUtil.printInfoLog(InfoLogs.PROCESSING_BATCHES.getLog());
         List<BulkInsertResponseRecord> records = new ArrayList<>();
+        List<CompletableFuture<BulkInsertResponse>> futures = this.insertBatchFutures(insertRequest, interceptor, cfg);
 
         try {
-            List<CompletableFuture<BulkInsertResponse>> futures = this.insertBatchFutures(insertRequest, interceptor, cfg);
             CompletableFuture<Void> allFutures = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
             try {
                 allFutures.join();
